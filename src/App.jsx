e9563f2d71ed3c190435137430b2925dbd304d1a
nurnewas <nurnewasjr@gmail.com>
2025-07-21 03:28:47 -0600

    using Basic of thiS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
 import "./App.css";
 
 function App() {
+  //Hard coded Js
+  function handleClick() {
+    alert("Ahaaaaa! uffff! I am clicked!");
+  }
 
+  const handleClick2 = () => {
+    alert("Ahaaaaa! uffff! I am also clicked! 2");
+  };
+
+  // add to five
+  const addToFive = (num) => {
+    alert(num + 5);
+  };
   return (
     <>
-      <h1>Hello </h1>
+      <h1>Core Concept </h1>
+      <button onClick={handleClick}>Use Me baby</button>
+      <button onClick={handleClick2}>touch me</button>
+      <button onClick={() => alert("From JSX")}>I'm From JSX</button>
+      <button onClick={() => addToFive(1)}>Adding 5</button>
     </>
   );
 }
